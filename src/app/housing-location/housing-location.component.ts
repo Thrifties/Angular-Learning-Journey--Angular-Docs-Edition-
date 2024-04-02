7d0794fbfb115b8b33ae447bf2089d7e8a861c6e
Kenshin <90311744+Thrifties@users.noreply.github.com>
2024-04-02 20:08:52 +0800

    Self-study for Angular framework

---
+++ src/app/housing-location/housing-location.component.ts
@@ -0,0 +1,18 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HousingLocation } from '../housing-location';
+
+@Component({
+  selector: 'app-housing-location',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      housing-location works!
+    </p>
+  `,
+  styleUrl: './housing-location.component.css'
+})
+export class HousingLocationComponent {
+  @Input() housingLocation!: HousingLocation;
+}
